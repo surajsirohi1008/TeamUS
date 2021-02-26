23317fc70adc8caf510d18bc522f93985fd71c66
Suraj Sirohi <surajsirohi1008@gmail.com>
2021-02-26 18:01:09 -0500

    Add Camera for Split Screen
    Add additional camera in the scene for second player's screen.

---
+++ CowGameSplitScreen/Assets/Scripts/Scr_Scene_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_Scene_Manager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
