281cd7a94fca1e95787abb5a010f6bf83c0e45e8
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2021-02-26 19:20:28 -0500

    Add Healthbar

---
+++ CowGameCopy- Gabe/Assets/Scripts/Scr_HealthUI_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_HealthUI_Controller : MonoBehaviour
+{
+    [SerializeField] private Image box, fill;
+    [SerializeField] private float widthInPixels = 200;
+    [SerializeField] private float heightInPixels = 50;
+    [SerializeField] private Scr_Movement_Controller scr_Movement_Controller;
+    private float maxHealth;
+    private float health;
+    void Start()
+    {
+        box.rectTransform.sizeDelta = new Vector2(widthInPixels, heightInPixels);
+        fill.rectTransform.sizeDelta = new Vector2(widthInPixels, heightInPixels);
+        maxHealth = scr_Movement_Controller.health;
+    }
+
+    void Update()
+    {
+        health = scr_Movement_Controller.health;
+        fill.rectTransform.sizeDelta = new Vector2(widthInPixels * (health / maxHealth), heightInPixels);
+    }
+}
--- CowGameCopy- Gabe/Assets/VertexColorFarmAnimals/Shaders/VertexColorUnlit.shader
+++ CowGameCopy- Gabe/Assets/VertexColorFarmAnimals/Shaders/VertexColorUnlit.shader
@@ -1,6 +1,7 @@
 ﻿Shader "VertexColorFarmAnimals/VertexColorUnlit" {
 Properties {
 	_MainTex ("Texture", 2D) = "white" {}
+	_Color("Color", Color) = (1,1,1,1)
 }
  
 Category {
